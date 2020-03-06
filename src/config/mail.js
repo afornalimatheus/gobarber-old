bbf3e61fbc42a283c355541bf441679d3d533ce7
Matheus Afornali <afornalimatheus@gmail.com>
2020-03-06 08:57:47 -0300

    - Enviando o e-mail quando acontece um cancelamento. Signed-off-by: Matheus Afornali <afornalimatheus@gmail.com>

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '185019622f39d2',
+    pass: 'dd68636c3cc495',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
